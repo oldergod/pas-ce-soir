5ee525dcc698dedd439a2ef9dc694b277495b6ef
benoit <oldering@gmail.com>
2016-06-09 11:45:46 +0900

    icons and manifest

--- sw.js
+++ sw.js
@@ -3,15 +3,23 @@ var CACHE_VERSION = '0.0.1';
 
 self.oninstall = function(event) {
   const urls = [
-    '/',
-    '/index.html',
-    '/styles/pas-ce-soir.css',
-    '/scripts/pas-ce-soir.js',
-    '/assets/pas-ce-soir.mp3',
-    '/assets/pas-maintenant.mp3',
-    '/assets/pas-aujourdhui.mp3',
-    '/assets/touche-pas.mp3',
-    '/manifest.json',
+    '.',
+    'index.html',
+    'styles/pas-ce-soir.css',
+    'scripts/pas-ce-soir.js',
+    'assets/pas-ce-soir.mp3',
+    'assets/pas-maintenant.mp3',
+    'assets/pas-aujourdhui.mp3',
+    'assets/touche-pas.mp3',
+    'assets/icons/icon-72x72.png',
+    'assets/icons/icon-96x96.png',
+    'assets/icons/icon-128x128.png',
+    'assets/icons/icon-144x144.png',
+    'assets/icons/icon-152x152.png',
+    'assets/icons/icon-192x192.png',
+    'assets/icons/icon-384x384.png',
+    'assets/icons/icon-512x512.png',
+    'manifest.json',
   ];
 
   event.waitUntil(

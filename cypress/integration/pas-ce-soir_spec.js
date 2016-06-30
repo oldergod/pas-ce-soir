ff66bc7d251fb788527f36e90bcbb309419edae1
benoit <oldering@gmail.com>
2016-06-30 16:19:05 +0900

    first integration tests with cypress

---
+++ cypress/integration/pas-ce-soir_spec.js
@@ -0,0 +1,46 @@
+describe('Pas ce soir !', function() {
+  beforeEach(function() {
+    cy.visit('/');
+  });
+  it('assert that <title> is correct', function() {
+    cy.title().should('include', 'Pas Ce Soir')
+  });
+
+  it('assert the DOM', function() {
+    cy.get('.action-container').children()
+      .should('have.length', 4)
+      .should('have.class', 'action')
+      .should(function(childrenList) {
+        var $childrenTexts = childrenList.map(function(_, child) {
+          return Cypress.$(child).text();
+        });
+        var childrenTexts = $childrenTexts.get();
+        expect(childrenTexts).to.have.length(4);
+        expect(childrenTexts).to.deep.eq([
+          'Pas ce soir',
+          'Pas maintenant',
+          'Pas aujourd\'hui',
+          'Touche pas',
+        ]);
+      });
+
+    cy
+      .get('.action-container')
+        .contains('Pas ce soir').should('have.class', 'action--pas-ce-soir')
+      .get('.action-container')
+        .contains('Pas maintenant').should('have.class', 'action--pas-maintenant')
+      .get('.action-container')
+        .contains('Pas aujourd\'hui').should('have.class', 'action--pas-aujourdhui')
+      .get('.action-container')
+        .contains('Touche pas').should('have.class', 'action--touche-pas')
+  });
+
+  it('assert click on actions', function() {
+    cy.get('.action-container').children()
+      .click({multiple: true});
+    cy.get('.action-container').children('.playing')
+      .should('have.length', 4);
+    cy.get('.action-container').children()
+      .wait(3000).should('not.have.class', 'playing');
+  });
+});
